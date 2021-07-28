322df93ab968ada71ecd3f794798b415f74a557f
banbanida <banban@packetplay.com>
2021-07-28 19:08:15 +0900

    Updates

---
+++ precache-manifest.85a6b49cb86517bf630f8660d4fe9161.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eab7e29b205972e44267a95ecb304ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ebbdd6fd3947c552f33",
+    "url": "./static/css/2.92a090a6.chunk.css"
+  },
+  {
+    "revision": "04851a37136814ad2c53",
+    "url": "./static/css/main.93cf3e71.chunk.css"
+  },
+  {
+    "revision": "6ebbdd6fd3947c552f33",
+    "url": "./static/js/2.bf9d58e3.chunk.js"
+  },
+  {
+    "revision": "aa94962c6278bf8f6df236d7f39cf3dc",
+    "url": "./static/js/2.bf9d58e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04851a37136814ad2c53",
+    "url": "./static/js/main.13548af3.chunk.js"
+  },
+  {
+    "revision": "3d17191187899b035115",
+    "url": "./static/js/runtime-main.5688f265.js"
+  }
+]);
